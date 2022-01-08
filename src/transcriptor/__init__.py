f73c7f94492613534e7e447e1af91e91cdfced1f
Jonnieey <johnjahi55@gmail.com>
2022-01-08 19:05:43 +0300

    refactor: Clean up

--- src/transcriptor/__init__.py
+++ src/transcriptor/__init__.py
@@ -1,8 +1,9 @@
 from appdirs import user_config_dir
+from pathlib import Path
 
-BASE_DIR = '/home/kamikaze/Documents/Wera/Transcription2'
-CLIENT_FOLDER = BASE_DIR + '/clients'
-JOB_FOLDER = BASE_DIR + '/jobs'
-WORK_FOLDER = BASE_DIR + '/work'
-CONF_FILE = user_config_dir('transcriptor')
+BASE_DIR = Path('/home/kamikaze/Documents/Wera/Transcription2')
+CLIENTS_FOLDER = BASE_DIR / 'clients'
+JOBS_FOLDER = BASE_DIR / 'jobs'
+WORKS_FOLDER = BASE_DIR / 'work'
+CONF_FOLDER = user_config_dir('transcriptor')
 DATE_FMT = '%Y-%m-%d'
