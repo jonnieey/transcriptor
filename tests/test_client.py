b6ca6304707709cde989291407c48a59e7c5b8e2
Jonnieey <johnjahi55@gmail.com>
2022-01-08 08:37:43 +0300

    test: Add clients more tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,12 +10,22 @@ def test_client():
     return Client(name=CLIENT_NAME, email=CLIENT_EMAIL)
 
 def test_client_to_dict(test_client):
-
-    client_json = {
+    client_dict = {
         "name": "TestClient",
         "email": "TestEmail",
     }
 
-    assert test_client.to_dict() == client_json
+    assert test_client.to_dict() == client_dict
 
+def test_client_to_json(test_client):
+    client_json = '{\n  "email": "TestEmail",\n  "name": "TestClient"\n}'
+
+    assert test_client.to_json() == client_json
+
+def test_client_from_json(test_client):
+    client_json = test_client.to_json()
+    client_obj = Client().from_json(client_json)
+
+    assert  isinstance(client_obj, Client)
+    assert client_obj.name == CLIENT_NAME
 
