7e4e3f25f42ba27c528341d7d6bc28e0e042a8bf
Jonnieey <johnjahi55@gmail.com>
2023-08-04 14:44:16 +0300

    refactor minor refactor

--- src/transcriptor/view.py
+++ src/transcriptor/view.py
@@ -10,8 +10,38 @@ class ConsoleView:
     def __init__(self):
         self.console = Console()
         self.table = Table(
-            show_header=True, header_style="bold red", title_justify="center"
+            show_header=True,
+            header_style="bold red",
+            title_justify="center",
+            padding=(0, 0),
         )
+
+    def generate_table(self, columns, rows):
+        # TODO let user choose to show job path in table
+        try:
+            col_amount = columns.index("amount")
+            col_amount_paid = columns.index("amount_paid")
+            total_amount = 0
+            total_amount_paid = 0
+            jobs_table = True
+        except ValueError:
+            jobs_table = False
+
+        for column in columns:
+            self.table.add_column(tc(column))
+        for row in rows:
+            if jobs_table:
+                total_amount += row[col_amount]
+                total_amount_paid += row[col_amount_paid]
+            row = list(map(str, row))
+            self.table.add_row(*row)
+
+        if jobs_table:
+            self.table.add_section()
+            summary_row = [""] * len(columns)
+            summary_row[col_amount] = str(round(total_amount, 2))
+            summary_row[col_amount_paid] = str(round(total_amount_paid, 2))
+            self.table.add_row(*summary_row)
 
     def print_table(self, data: dict | list, orientation: str = "vert") -> None:
         """
@@ -21,33 +51,6 @@ class ConsoleView:
             data: Data to print
             orientation: Orientation of the table (vert or hor)
         """
-
-        # TODO let user choose to show job path in table
-        def generate_table(columns, rows):
-            try:
-                col_amount = columns.index("amount")
-                col_amount_paid = columns.index("amount_paid")
-                total_amount = 0
-                total_amount_paid = 0
-                jobs_table = True
-            except ValueError:
-                jobs_table = False
-
-            for column in columns:
-                self.table.add_column(tc(column))
-            for row in rows:
-                if jobs_table:
-                    total_amount += row[col_amount]
-                    total_amount_paid += row[col_amount_paid]
-                row = list(map(str, row))
-                self.table.add_row(*row)
-
-            if jobs_table:
-                self.table.add_section()
-                summary_row = [""] * len(columns)
-                summary_row[col_amount] = str(round(total_amount, 2))
-                summary_row[col_amount_paid] = str(round(total_amount_paid, 2))
-                self.table.add_row(*summary_row)
 
         if not data:
             return
@@ -73,11 +76,12 @@ class ConsoleView:
                     columns = list(data[0].keys())
                     with contextlib.suppress(ValueError):
                         columns.remove("job_path")
-                    rows = [[row[column] for column in columns] for row in data]
-                    generate_table(columns, rows)
+                    rows = [[row[column] for column in columns]
+                            for row in data]
+                    self.generate_table(columns, rows)
                 except AttributeError:
                     columns = data[0]
                     rows = data[1:]
-                    generate_table(columns, rows)
+                    self.generate_table(columns, rows)
 
         self.console.print(self.table)
